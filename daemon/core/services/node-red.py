6bee93fb61b1e8831ee6df95208f1ae05852506f
Andreas Martens <amartens@uk.ibm.com>
2018-09-04 16:50:15 +0100

    put a default flow into Node-RED service

--- daemon/core/services/node-red.py
+++ daemon/core/services/node-red.py
@@ -34,7 +34,7 @@ class MyService(CoreService):
         Return a string that will be written to filename, or sent to the
         GUI for user customization.
         """
-        cfg = """
+        cfg = """[{"id":"5a373772.6f6b98","type":"tab","label":"Flow 1","disabled":false,"info":""},{"id":"bac2efc.047b89","type":"mqtt-broker","z":"","name":"","broker":"localhost","port":"1883","clientid":"","usetls":false,"compatmode":false,"keepalive":"60","cleansession":true,"birthTopic":"","birthQos":"0","birthPayload":"","closeTopic":"","closeQos":"0","closePayload":"","willTopic":"","willQos":"0","willPayload":""},{"id":"c57e9724.5b45c8","type":"mqtt in","z":"5a373772.6f6b98","name":"","topic":"#","qos":"2","broker":"bac2efc.047b89","x":240,"y":260,"wires":[["d28d76c1.0c1628"]]},{"id":"d28d76c1.0c1628","type":"debug","z":"5a373772.6f6b98","name":"","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"false","x":510,"y":260,"wires":[]}]
 """
         return cfg
 
